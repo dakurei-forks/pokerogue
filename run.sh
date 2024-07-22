cb0e4136ef47b355b5a11ede2015c504dbfc4299
Dakurei <maxime.palanchini@gmail.com>
2024-07-22 10:55:29 +0200

    Add script to build app and generate manifest.json directly into Nginx folder

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run --rm -v `pwd`:/app/:rw -v /var/www/pokerogue.dakurei.ovh/:/app/dist/:rw -w /app/ node:20.13.1-alpine sh -c "npm ci && npm run build"
+docker run --rm -v `pwd`:/app/:rw -v /var/www/pokerogue.dakurei.ovh/:/app/dist/:rw -w /app/ ruby:3.3.1-alpine sh -c "ruby manifest_generator.rb -v --inpath ./dist/"
